9eccc12e7f1d908d3c66930844f72f9419ffb214
Roman Artiukhin <bahusdrive@gmail.com>
2024-11-13 00:05:53 +0200

    Avoid filename string allocation

--- __init__.py
+++ __init__.py
@@ -4,8 +4,8 @@ from cudatext import *
 class Command:
     
     def on_open_pre(self, ed_self, filename):
-        ext = os.path.basename(filename).split('.')[-1].lower()
-        if ext in ('jpg', 'jpeg', 'png', 'bmp', 'ico'):
+        ext = filename.rfind('.') + 1
+        if  ext != 0 and filename[ext:].lower() in ('jpg', 'jpeg', 'png', 'bmp', 'ico'):
             print('Ignoring picture:', filename)
             file_open(filename, options='/view-hex')
             return False
